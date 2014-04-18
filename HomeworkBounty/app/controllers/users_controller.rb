1496ea2b02ecb9f3dfbe357ab43895577a0b2964
Kim <kimpersson88@gmail.com>
2014-04-18 22:41:14 +0200

    basic registration works, but doesn't show errors to user

--- HomeworkBounty/app/controllers/users_controller.rb
+++ HomeworkBounty/app/controllers/users_controller.rb
@@ -10,15 +10,19 @@ class UsersController < ApplicationController
 	def create
 		user_params = params[:user].permit( :username, :password, :school, :email )
 		school = School.find(user_params[:school])
+		error = false
 		begin
-			@user = school.students.create(user_params)
-		rescue => e
-			error = True
+			user = school.students.create(user_params)
+			if user.persisted?
+				@user = user
+				render :json => @user
+			else
+				redirect_to '/register'
+			end
+		rescue Moped::Errors::OperationFailure 
+			if [11000, 11001].include?(e.details['code'])
+				# Duplicate key error show user that they need to choose another username
+			end
 		end
-    if @user and not error
-      render nothing: true
-    else
-      render nothing: true
-    end
 	end
 end
