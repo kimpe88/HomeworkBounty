63abdabbc268dfe3fdcdd843cf5977edbcf53e2e
Kim <kimpersson88@gmail.com>
2014-04-21 19:35:49 +0200

    Fixed error messages for registration

--- HomeworkBounty/app/controllers/users_controller.rb
+++ HomeworkBounty/app/controllers/users_controller.rb
@@ -1,23 +1,29 @@
 class UsersController < ApplicationController
-	def new 
+	def create_sessions_var
 		@schools = [] 
 		School.all.each do |school| 	
 			@schools << school.name
 		end
 		@user = User.new
+	end
+	def new 
+		self.create_session_var
+		puts "USER STATUS " + @user 
 		render 'register_user'
 	end
 	def create
+		if @user.nil?
+			self.create_sessions_var
+		end
 		user_params = params[:user].permit( :username, :password, :school, :email )
 		school = School.find(user_params[:school])
 		error = false
 		begin
-			user = school.students.create(user_params)
-			if user.persisted?
-				@user = user
+			@user = school.students.create(user_params)
+			if @user.persisted?
 				render :json => @user
 			else
-				redirect_to '/register'
+				render 'register_user'
 			end
 		rescue Moped::Errors::OperationFailure 
 			if [11000, 11001].include?(e.details['code'])
--- HomeworkBounty/app/models/user.rb
+++ HomeworkBounty/app/models/user.rb
@@ -9,16 +9,17 @@ class User
   # Uses secure password from active model
   # http://apidock.com/rails/ActiveModel/SecurePassword/ClassMethods/has_secure_password
   field :password_digest, type: String
-	validates :password, :presence => true,
-											 :allow_blank => false,
-											 :length => {:minimum => 6}
+	validates :password, :length => {:minimum => 6, :message => " minmum length is 6 characters"}
   has_secure_password
   belongs_to :school, :class_name => 'School'
 
   def student_email
 		if not self.email =~ /#{school.email_domain}$/i
-  		errors[:field] << "Email does not match school email domain"
+  		errors[:Email] << "does not match school email domain"
   	end
-  	
+	end
+
+	def to_s
+		"Username #{username}, email #{email}, password #{password}"
 	end
 end
